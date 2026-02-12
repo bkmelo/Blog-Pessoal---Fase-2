2aa62799d507506088be5c2c845417e3bbaca80d
Bruna Melo <bmelo.silva25@gmail.com>
2026-02-12 13:27:05 -0300

    Navbar e Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Navbar from './components/navbar/Navbar';
 import Home from './pages/home/Home';
+import Footer from './components/footer/Footer';
 
 function App() {
     return(
-      <Home/>
+      <>
+
+      <BrowserRouter>
+      <Navbar />
+      <div className="min-h-[80vh]">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+        </div>
+      <Footer />
+      </BrowserRouter>
+      
+      </>
     );
 }
 
