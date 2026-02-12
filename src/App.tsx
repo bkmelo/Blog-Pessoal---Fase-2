f81dca5856c4f84e04d6cfb8a31cc670f978cbc5
Bruna Melo <bmelo.silva25@gmail.com>
2026-02-12 17:00:35 -0300

    rotas

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import Navbar from './components/navbar/Navbar';
 import Home from './pages/home/Home';
 import Footer from './components/footer/Footer';
+import Cadastro from './pages/cadastro/Cadastro';
+import Login from './pages/login/Login';
+
 
 function App() {
     return(
@@ -11,8 +14,9 @@ function App() {
       <Navbar />
       <div className="min-h-[80vh]">
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Login />} />
           <Route path="/home" element={<Home />} />
+          <Route path="/cadastro" element={<Cadastro />} />
         </Routes>
         </div>
       <Footer />
