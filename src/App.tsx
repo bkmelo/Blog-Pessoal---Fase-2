093c87923458ef4835094c795dc4f9f850383d4a
Bruna Melo <bmelo.silva25@gmail.com>
2026-02-13 16:46:23 -0300

    Implementação das models

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,27 @@ import Home from './pages/home/Home';
 import Footer from './components/footer/Footer';
 import Cadastro from './pages/cadastro/Cadastro';
 import Login from './pages/login/Login';
+import { AuthProvider } from './contexts/AuthContext';
 
 
 function App() {
     return(
       <>
 
+      <AuthProvider>
       <BrowserRouter>
       <Navbar />
       <div className="min-h-[80vh]">
         <Routes>
           <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login/>}/>
           <Route path="/home" element={<Home />} />
           <Route path="/cadastro" element={<Cadastro />} />
         </Routes>
         </div>
       <Footer />
       </BrowserRouter>
+      </AuthProvider>
       
       </>
     );
---
+++ src/models/UsuarioLogin.ts
@@ -0,0 +1,10 @@
+
+
+export interface UsuarioLogin{
+  id: number;
+  nome: string;
+  usuario: string;
+  senha: string;
+  foto:string;
+  token: string;
+}
